ea6f619e5cb05cb01e0588c13266cbc40cbcbf04
Ta Ngoc Qui <tangocqui010@gmail.com>
2019-08-19 15:51:57 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'verruni' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'qui0904436284' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
